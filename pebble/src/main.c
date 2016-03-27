a8257479906888d6c40e47f031daef93074a64ee
bb-froggy <ch-git@hannebauer.name>
2016-03-27 14:44:49 +0200

    DataLogging API

--- pebble/src/main.c
+++ pebble/src/main.c
@@ -3,7 +3,11 @@
 #include "windows/main_window.h"
 
 static void init() {
+#ifdef USE_DATALOGGING
+  commlog_init();
+#else
   comm_init();
+#endif
   main_window_push();
 }
 
--- pebble/src/windows/main_window.h
+++ pebble/src/windows/main_window.h
@@ -1,7 +1,17 @@
 #pragma once
 
+#define USE_DATALOGGING
+
 #include <pebble.h>
 
+#ifdef USE_DATALOGGING
+#include "../modules/commlog.h"
+#define SEND_DATA commlog_send_data
+#define IS_BUSY commlog_is_busy
+#else
 #include "../modules/comm.h"
+#define SEND_DATA comm_send_data
+#define IS_BUSY comm_is_busy
+#endif
 
 void main_window_push();
